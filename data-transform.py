75814db6702de1278adfbf6d16452caae2f3635d
hypeng <wzkg2012@sina.cn>
2016-12-23 23:08:23 +0800

    resize image to 224x224
    resize image to 224x224 using  linear interpolation

--- data-transform.py
+++ data-transform.py
@@ -3,6 +3,7 @@ import cPickle
 import numpy as np
 import logging
 import pdb
+import cv2
 
 logger = logging.getLogger(__name__)
 def unpickle( baseName):
@@ -20,8 +21,8 @@ def futz(X):
 if __name__ == '__main__':
 	fnames = ['data_batch_%i'%i for i in range(1,6)]
 	#fnames.append('test_batch')
-
-	pixel = np.zeros((50000, 3,32,32), dtype='uint8')
+        size = 224
+	pixel = np.zeros((50000, 3,224,224), dtype='uint8')
 	labels = np.zeros(50000, dtype='int32')
 	tmpData = np.zeros((10000,3,32,32), dtype='uint8')
 	tmpLabel = np.zeros(10000, dtype='int32')
@@ -34,9 +35,9 @@ if __name__ == '__main__':
 		tmpLabel = data['labels']
 		for j in range(len(tmpLabel)):
 			index = startPtr[tmpLabel[j]]
-			pixel[index] = tmpData[j]
+			pixel[index] = cv2.resize(tmpData[j][0,:,:], dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
 			startPtr[tmpLabel[j]] = index+1
-	
+				
 
 	for i in range(len(LABELS)):
 		np.save(LABELS[i]+'.npy',pixel[i*5000:5000*(i+1)])
